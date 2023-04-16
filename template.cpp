7bd16f3787aa6557dca7e8febe17eda1dfca32b7
SM-74 <73398148+SM-74@users.noreply.github.com>
2023-04-16 19:32:10 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define ll long long
+#define vi vector<int>
+#define ui unsigned int
+#define vii vector<vector<int>>
+#define pb push_back
+#define endl "\n"
+#define sortall(a) sort(a.begin(),a.end())
+#ifdef LOCAL
+#define deb(x) cout <<'\t'<< #x <<" = "<<x<<endl;
+#define deb2(x,y) cout <<'\t'<< #x <<":"<<x<<" " <<#y<<":"<<y<<endl;
+#endif
+#ifndef LOCAL
+#define deb(x)
+#define deb2(x,y)
+#endif
+using namespace std;
+
+
+
+void solve() {
+
+
+
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
